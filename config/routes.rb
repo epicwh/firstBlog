2ae4a7d626fb27fa2df0a45fd60c89050db429d8
epicwh <sapsan1996@yandex.ru>
2018-11-05 21:04:56 +0300

    second lesson results

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'ololo/home/index', to: 'home#index'
 end
+
+#http://localhost:3000/home/index
